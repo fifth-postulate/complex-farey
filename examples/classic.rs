5c4f0e18a76716e0f9414ece997b75fd29b1d569
Daan van Berkel <daan.van.berkel@alliander.com>
2023-07-31 14:51:06 +0200

    Allow multiple runs of the mediants

--- examples/classic.rs
+++ examples/classic.rs
@@ -1,14 +1,34 @@
 use farey::farey::Fraction;
 
 pub fn main() {
-    let start: Vec<Fraction<u64>> = vec![(0, 1).into(), (1,1).into()];
-    let a = start.iter();
-    let mut b = start.iter();
+    let start: Vec<Fraction<u64>> = vec![(0, 1).into(), (1, 1).into()];
 
-    b.next();
-    
-    let result: Vec<Fraction<u64>> = a.zip(b).map(|(l,r)| l.mediant(*r)).collect();
+    let mut current = start;
+    for _ in 0..5 {
+        current = mediants(current);
+    }
 
+    for item in current {
+        print!("{} ", item);
+    }
+}
 
-    println!("{:?}", result)
+fn mediants(original: Vec<Fraction<u64>>) -> Vec<Fraction<u64>> {
+    let mut result = Vec::new();
+
+    for index in 0..(original.len() - 1) {
+        let first = original[index];
+        let second = original[index + 1];
+
+        result.push(first);
+        result.push(first.mediant(second));
+    }
+    result.push(
+        original
+            .last()
+            .expect("original to have a last element")
+            .clone(),
+    );
+
+    result
 }
